5a3627725e7b29639ff0ece1b3a7798d4bb5c04c
hj-66 <a01088200534@gmail.com>
2026-07-16 14:29:39 +0900

    코틀린으로 변환

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
-    java
-    id("org.springframework.boot") version "4.0.6"
+    kotlin("jvm") version "2.3.21"
+    kotlin("plugin.spring") version "2.3.21"
+    id("org.springframework.boot") version "4.1.0"
     id("io.spring.dependency-management") version "1.1.7"
+    kotlin("plugin.jpa") version "2.3.21"
 }
 
 group = "com"
@@ -22,15 +24,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-h2console")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
-    compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
-    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    testCompileOnly("org.projectlombok:lombok")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testAnnotationProcessor("org.projectlombok:lombok")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    implementation(kotlin("stdlib"))
 }
 
 tasks.withType<Test> {
---
+++ src/main/java/com/back/domain/post/post/entity/Post.kt
@@ -0,0 +1,18 @@
+package com.back.domain.post.post.entity
+
+import com.back.domain.member.member.entity.Member
+import com.back.global.jpa.entity.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.ManyToOne
+
+@Entity
+class Post(
+    @field:ManyToOne
+    var author: Member? = null,
+
+    var title: String,
+
+    @field:Column(columnDefinition = "TEXT")
+    var content: String
+) : BaseEntity()
